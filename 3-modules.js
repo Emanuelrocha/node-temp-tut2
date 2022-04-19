e273700bf925d48b5e79468098386b1d3475d959
Emanuel Rocha <rochaslm@gmail.com>
2022-04-19 17:14:34 -0500

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,21 @@
+// const amount = 12
+
+// amount <10 ? console.log('small number') : console.log('large number')
+
+// console.log('first shit')
+
+// console.log(__dirname)
+// setInterval(()=>{
+//     console.log('hello, world')
+// },1000)
+
+// const john = 'john'
+// const peter = 'peter'
+
+// const sayHi = (name) =>{
+//     console.log(`hello there, ${name}`)
+// }
+
+// sayHi('susan')
+// sayHi(john)
+
